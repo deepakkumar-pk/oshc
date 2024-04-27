c215fb51455c05c51828196c2ff7f7d950acff4a
Deepak Kumar <kingdk0035@gmail.com>
2024-04-27 15:01:24 +0500

    sample

--- src/app/page.js
+++ src/app/page.js
@@ -1,113 +1,156 @@
+"use client";
 import Image from "next/image";
+import React, { useState, useEffect } from "react";
+import { BsFillQuestionCircleFill } from "react-icons/bs";
+import { RiInformation2Fill } from "react-icons/ri";
 
-export default function Home() {
+
+const Page = () => {
+  const [startDate, setStartDate] = useState("");
+  const [endDate, setEndDate] = useState("");
+
+  useEffect(() => {
+    let today = new Date();
+
+    let nextMonth = new Date(today.getFullYear(), today.getMonth() + 1, 1);
+
+    let nextYear = new Date(
+      today.getFullYear() + 2,
+      nextMonth.getMonth(),
+      nextMonth.getDate() - 1
+    );
+
+    let nextMonthFormatted = formatDate(nextMonth);
+    let nextYearFormatted = formatDate(nextYear);
+
+    setStartDate(nextMonthFormatted);
+    setEndDate(nextYearFormatted);
+  }, []);
+
+  const formatDate = (date) => {
+    let year = date.getFullYear();
+    let month = (date.getMonth() + 1).toString().padStart(2, "0");
+    let day = date.getDate().toString().padStart(2, "0");
+    return `${year}-${month}-${day}`;
+  };
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
+    <div className="bg-base h-[60vh] bg-cover bg-no-repeat flex justify-center items-center">
+      <div className="max-w-[50rem]">
+        <div className="bg-white px-10 py-6 bg-opacity-90 rounded-t-md">
+          <div>
             <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
+              src="/logo.svg"
+              width={800}
+              height={800}
+              alt="Picture of the author"
             />
-          </a>
+            <h3 className="text-right font-bold text-[#888888] -mt-6 text-lg">
+              Get the best deal on OSHC
+            </h3>
+          </div>
+          <div className="pt-4 space-y-3">
+            <p className="text-center font-normal text-[#333333]">
+              You have a choice in your overseas student health cover. Let OSHC
+              Australia help you compare your OSHC insurance options, and find
+              the best value insurance for you.
+            </p>
+            <h3 className="text-center font-medium text-black text-[22px]">
+              Compare Australian Government-approved providers in one place; buy
+              your policy and have it in minutes.
+            </h3>
+          </div>
+        </div>
+        <div className="">
+          <div className="bg-[#0297C4] bg-opacity-80 px-8 py-4">
+            <div className="grid grid-cols-3 space-x-2">
+              <div className="col-span-1 flex justify-center items-center">
+                <div className="w-full grid place-items-center space-y-1">
+                  <label htmlFor="adults" className="text-xs text-white ">
+                    Adults
+                  </label>
+                  <select
+                    id="adults"
+                    name="adults"
+                    className="text-sm text-[#555555] w-24 px-1 text-center py-1 rounded-sm"
+                  >
+                    <option value="1">1 Adult</option>
+                    <option value="2">2 Adults</option>
+                  </select>
+                </div>
+                <div className="w-full grid place-items-center space-y-1">
+                  <label htmlFor="children" className="text-xs text-white">
+                    Children
+                  </label>
+                  <select
+                    id="children"
+                    name="children"
+                    className="text-sm text-[#555555] w-28 p-1 text-center rounded-sm"
+                  >
+                    <option value="0">0 Children</option>
+                    <option value="1">1 Child</option>
+                    <option value="2">2 Children</option>
+                    <option value="3">3 Children</option>
+                    <option value="4">4 Children</option>
+                    <option value="5">5 Children</option>
+                    <option value="6">6 Children</option>
+                    <option value="7">7 Children</option>
+                    <option value="8">8 Children</option>
+                    <option value="9">9 Children</option>
+                    <option value="10">10 Children</option>
+                  </select>
+                </div>
+              </div>
+
+              <div className="col-span-2 flex items-center justify-between">
+                <div className="w-full grid place-items-center space-y-1">
+                  <label
+                    htmlFor="start-date"
+                    className="text-xs text-white flex"
+                  >
+                    My policy starts on
+                    <BsFillQuestionCircleFill className="text-sm ml-1" />
+                  </label>
+                  <input
+                    type="date"
+                    id="start-date"
+                    name="start-date"
+                    value={startDate}
+                    onChange={(e) => setStartDate(e.target.value)}
+                    className="text-sm text-[#555555] w-52 p-1 text-center rounded-sm"
+                  />
+                </div>
+                <div className="w-full grid place-items-center space-y-1">
+                  <label htmlFor="end-date" className="text-xs text-white flex">
+                    My policy finishes on
+                    <BsFillQuestionCircleFill className="text-sm ml-1" />
+                  </label>
+                  <input
+                    type="date"
+                    id="end-date"
+                    name="end-date"
+                    value={endDate}
+                    onChange={(e) => setEndDate(e.target.value)}
+                    className="text-sm text-[#555555] w-52 p-1 text-center rounded-sm"
+                  />
+                </div>
+              </div>
+            </div>
+            <div className="grid place-items-center mt-3">
+              <button className="text-[28px] px-4 py-1 rounded-md bg-[#28a745] text-white hover:bg-green-800">
+                Get Quote Now
+              </button>
+            </div>
+          </div>
+          <div className="bg-black opacity-80 text-[11px] rounded-b-md text-white py-3 text-center flex justify-center items-center">
+            <RiInformation2Fill className="mr-1 text-2xl"/>
+            OSHC is a visa-length cover - you need to get a policy that covers
+            the entire length of your student visa.
+          </div>
         </div>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    </div>
   );
-}
+};
+
+export default Page;
